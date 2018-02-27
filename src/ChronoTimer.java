3860a416632f4d703f7e7a7c9a96c84cecd34a81
Luke Stoltenberg <lukestolt@gmail.com>
2018-02-26 21:21:35 -0600

    added stuff to the Simulation aswell as the overloaded method to chronotimer

--- src/ChronoTimer.java
+++ src/ChronoTimer.java
@@ -21,18 +21,19 @@ public class ChronoTimer {
         for(int i = 0; i < 7; ++i) {
             channels[i] = new Channel(i+1);
         }
+        sysTime = new Time();
         eventList = new ArrayList<>();
 
     }
 
     public void execute(String command, String value){
+
         switch(command)
         {
             case "POWER":
                 if(curState.equals(State.OFF)){
                     curState = State.ON;
                     //setting the interal clock
-                    sysTime = new Time();
                 }
                 else
                     curState = State.OFF;
@@ -99,7 +100,7 @@ public class ChronoTimer {
                 if(curState.equals(State.INPROGRESS)){
                     //print the results of the race
                     //send to simulation to print
-                    execute("PRINT",event.printResults());
+                    Simulation.execute("PRINT",event.printResults());
                 }
                 break;
             case "ENDRUN":
@@ -107,8 +108,7 @@ public class ChronoTimer {
                     //go to the ON state when the run is over so that there can be another run
                     eventList.add(event);
                     curState = State.ON;
-                    //print all the racers after the run
-
+                    System.out.println(event.printResults());
                 }
                 break;
             case "DNF":
@@ -120,6 +120,117 @@ public class ChronoTimer {
             case "CANCEL":
                 if(curState.equals(State.INPROGRESS)){
                    //put the racer back in the queue at the beginning
+                    event.cancelRacer();
+                }
+                break;
+            case "EXIT":
+                if(curState.equals(State.INPROGRESS)){
+                    //just reset the fields so its like restarting at
+                    System.out.println("EXITING SIMULATOR...");
+                }
+                break;
+            case "TIME":
+                if(curState.equals(State.INPROGRESS)){
+                    //when can TIME be called?
+                    //do something with the system time
+                }
+                break;
+        }
+
+    }
+    public void execute(String time, String command, String value){
+        switch(command)
+        {
+            case "POWER":
+                if(curState.equals(State.OFF)){
+                    curState = State.ON;
+                    //setting the interal clock
+                }
+                else
+                    curState = State.OFF;
+                break;
+            case "EVENT":
+                if(curState.equals(State.ON)) {
+                    event = new Event(channels);
+                    curState = State.EVENT;
+                }
+                break;
+            case "NEWRUN":
+                if(curState.equals(State.EVENT)){
+                    curState = State.RUN;
+                }
+                break;
+            case "TOG":
+                if(curState.equals(State.RUN)){
+                    //-1 for the index
+                    int channelIndex = Integer.parseInt(value) - 1;
+                    channels[channelIndex].toggle();
+
+                    curState = State.TOG;
+                }
+                else if(curState.equals(State.TOG)){
+                    int channelIndex = Integer.parseInt(value) - 1;
+                    channels[channelIndex].toggle();
+                    curState = State.INPUTRACERS;
+                }
+                break;
+            case "NUM":
+                if(curState.equals(State.INPUTRACERS) || curState.equals(State.RUN)){
+                    event.addRacer(Integer.parseInt(value));
+                    //dont change the state because may need to enter multiple racers.
+                }
+                break;
+            case "TRIG":
+                if(curState.equals(State.INPUTRACERS) || curState.equals(State.INPROGRESS)){
+                    int channelNum = Integer.parseInt(value);
+
+                    //if it is odd then it is the start
+                    if((channelNum % 2) != 0) {
+                        event.setStartTime(time);
+                        curState = State.INPROGRESS;
+                    }
+                    //only finish if the there was already a start
+                    else if(curState.equals(State.INPROGRESS))
+                        event.setFinishTime(System.currentTimeMillis());
+                }
+                break;
+            //same as TRIG 1
+            case "START":
+                if(curState.equals(State.INPUTRACERS) || curState.equals(State.INPROGRESS)){
+                    event.setStartTime(System.currentTimeMillis());
+                    curState = State.INPROGRESS;
+                }
+                break;
+            //same as TRIG 2
+            case "FINISH":
+                if(curState.equals(State.INPUTRACERS)){
+                    event.setFinishTime(System.currentTimeMillis());
+                }
+                break;
+            case "PRINT":
+                if(curState.equals(State.INPROGRESS)){
+                    //print the results of the race
+                    //send to simulation to print
+                    Simulation.execute("PRINT",event.printResults());
+                }
+                break;
+            case "ENDRUN":
+                if(curState.equals(State.INPROGRESS)){
+                    //go to the ON state when the run is over so that there can be another run
+                    eventList.add(event);
+                    curState = State.ON;
+                    System.out.println(event.printResults());
+                }
+                break;
+            case "DNF":
+                if(curState.equals(State.INPROGRESS)){
+                    //assign the next up racer the DNF tag represented by -1 right now
+                    event.setFinishTime(-1);
+                }
+                break;
+            case "CANCEL":
+                if(curState.equals(State.INPROGRESS)){
+                    //put the racer back in the queue at the beginning
                     event.cancelRacer();
                 }
                 break;
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,16 +1,57 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.io.File;
 public class Simulation {
     public static void main(String[] args) {
-        ChronoTimer c = new ChronoTimer();
+        ChronoTimer chronoTimer = new ChronoTimer();
         Scanner stdIn = new Scanner(System.in);
 
+
+        //
+        String s;
+        do{
+            System.out.print("[f]ile or [c]onsole: ");
+            s = stdIn.nextLine();
+        }while(!(s.equalsIgnoreCase("f") || s.equalsIgnoreCase("c")));
+        boolean addTimeStamp = true;
+        if(s.equalsIgnoreCase("f")){
+            // File
+            try{
+                stdIn = new Scanner(new File("input.txt"));
+
+                while(stdIn.hasNext()){
+                    String str = stdIn.nextLine();
+                    System.out.println(str);
+                    String[] strArr = str.split(" ");
+                    if(strArr.length < 3)
+                        chronoTimer.execute(strArr[0], strArr[1]);
+                    else if(strArr.length == 3){
+                        chronoTimer.execute(strArr[0], strArr[1],strArr[2]); //0 - Time, 1- - Command, 2 - Value
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        } else{
+            // Console
+            while(true){
+                String[] str = stdIn.nextLine().split(" ");
+                if(str.length < 3)
+                    chronoTimer.execute(str[0], str[1]);
+                else if(str.length == 3){
+                    chronoTimer.execute(str[0], str[1],str[2]); //0 - Time, 1- - Command, 2 - Value
+                }
+            }
+        }
+        stdIn.close();
     }
 
-    public void execute(String command, String value){
+    public static void execute(String command, String value){
         if(command.equalsIgnoreCase("PRINT")){
             System.out.println(value);
         }
 
     }
+
 }
