f4716f65413d2ffe79fd3a02f9776bfec20b0e92
Luke <stolten6@uwm.edu>
2018-02-28 10:56:13 -0600

    added some test for Racer

--- src/Testing/TestRacer.java
+++ src/Testing/TestRacer.java
@@ -3,11 +3,23 @@ import TimingSystem.Racer;
 import org.junit.Test;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestRacer {
+    Racer r1 = new Racer(1);
+    Racer r2 = new Racer(2);
+
     @Test
-    public void testa(){
-        assertTrue("True", 1==1);
+    public void test1(){
+        assertEquals("The Racer had the wrong bib number",1,r1.getBibNumber());
+        assertEquals("The Racer had the wrong bib number",2,r2.getBibNumber());
+    }
+    @Test
+    public void test2(){
+        //-1 represents DNF
+        assertEquals("Wrong finish time",-1,r1.getFinishTime());
+        assertEquals("Wrong finish time",-1,r1.getStartTime());
+
     }
 }
