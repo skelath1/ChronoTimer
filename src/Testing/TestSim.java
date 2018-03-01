64224d7cb997d5358c5683995bbcabab41a1ba7a
Matt Fox <fox5@uwm.edu>
2018-02-28 22:49:48 -0600

    updated test sim

--- src/Testing/TestSim.java
+++ src/Testing/TestSim.java
@@ -2,13 +2,31 @@ package Testing;
 import org.junit.*;
 import Util.*;
 import TimingSystem.*;
-i
+
 public class TestSim {
 
 
     @Test
     public void testSIM(){
-        ChronoTimer = new ChronoTimer();
+        ChronoTimer ch = new ChronoTimer();
+        ch.execute("12:14:15.222", "POWER", null);
+        ch.execute("EVENT IND", null);
+        ch.execute("0:14:15:223", "NEWRUN", null);
+        ch.execute("tOG","1" );
+        ch.execute("Tog", "2");
+        ch.execute("1:15:16.355", "NUM", "234");
+        ch.execute("NUM", "366");
+        ch.execute("TRIG", "1");
+        ch.execute("TRIG", "3");
+        ch.execute("tOG","1" );
+        ch.execute("2.33.27.345","NUM", "667");
+        ch.execute("TRIG", "1");
+        ch.execute("triG","2" );
+        ch.execute("TriG","4" );
+        ch.execute("TRIG", "3");
+        ch.execute("3:43:23.351", "PRINT", null);
+        ch.execute("ENDRUN", null);
+        ch.execute("EXIT", null);
     }
 
 }
