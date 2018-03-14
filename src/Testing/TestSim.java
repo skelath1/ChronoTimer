805a7b6a0485ee44c5e24e6ffb942b6417592d6b
Matt Fox <fox5@uwm.edu>
2018-03-13 21:35:49 -0500

    added tests to test sim

--- src/Testing/TestSim.java
+++ src/Testing/TestSim.java
@@ -44,7 +44,7 @@ public class TestSim {
         ch.execute("EXIT", null);
     }
     @Test
-    public void testSIM2(){//test EVENT IND -> unneccessary time and commands
+    public void testSIM2(){//test EVENT IND -> time change during triggers
         ChronoTimer ch = new ChronoTimer();
         ch.execute("00:00:00.0", "POWER", null);
         ch.execute("EVENT", "IND");
@@ -53,14 +53,86 @@ public class TestSim {
         ch.execute("Tog", "2");
         ch.execute("NUM", "234");
         ch.execute("TRIG", "1");
-        //changing time over 100 hrs
+        //changing time
         ch.execute("TIME","02:00:01.0");
         ch.execute("TIME","03:00:00.9");
         ch.execute("TIME","04:00:00.8");
         ch.execute("TIME","05:00:00.7");
-        //time limit reached
+        //time changed
        ch.execute("TRIG", "2");
        ch.execute("PRINT", null);
 
+    }
+    @Test
+    public void testSIM3(){//test EVENT PARIND -> multi events
+        ChronoTimer ch = new ChronoTimer();
+        ch.execute("3:00:00.0", "POWER", null);
+        ch.execute("TIME","4:00:00.2");
+        ch.execute("EVENT", "PARIND");
+        ch.execute("NEWRUN", null);
+        ch.execute("tOG","1" );
+        ch.execute("tOG","1" );
+        ch.execute("tOG","1" );
+        ch.execute("Tog", "2");
+        ch.execute("tOG","4" );
+        ch.execute("tOG","3" );
+        ch.execute("NUM", "55");
+        ch.execute("NUM", "56");
+        ch.execute("NUM", "57");
+        ch.execute("NUM", "58");
+        ch.execute("TRIG", "1");
+        try{
+            Thread.sleep(1000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "3");
+        try{
+            Thread.sleep(1000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "3");
+        try{
+            Thread.sleep(1000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "1");
+        ch.execute("TRIG", "2");
+        try{
+            Thread.sleep(1000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "2");
+        ch.execute("TRIG", "4");
+        try{
+            Thread.sleep(1000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "4");
+        ch.execute("PRINT", null);
+        ch.execute("ENDRUN", null);
+        ch.execute("EVENT", "IND");
+        ch.execute("NEWRUN", null);
+        ch.execute("tOG","1" );
+        ch.execute("Tog", "2");
+        ch.execute("NUM", "234");
+        ch.execute("TRIG", "1");
+        ch.execute("TRIG", "1");
+        try{
+            Thread.sleep(3000);
+        }catch(Exception ex){
+            System.out.println("Interrupted...");
+        }
+        ch.execute("TRIG", "2");
+        ch.execute("PRINT", null);
+        ch.execute("ENDRUN", null);
+        ch.execute("EXPORT","1" );
+        ch.execute("EXPORT", "2");
+        ch.execute("POWER", null);
+        ch.execute("EXIT", null);
     }
 }
