c5917ab9f650593187cc182d4cb5306a58107649
Frederick Barthel <fbarthel@uwm.edu>
2018-04-18 14:00:40 -0500

    updated chronogoo: backside gooey corrected channel options. Started backSide back end

--- src/GUI/BackSide.java
+++ src/GUI/BackSide.java
@@ -1,22 +1,40 @@
 package GUI;
 
+import TimingSystem.ChronoTimer;
+import TimingSystem.Simulation;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BackSide {
     private JPanel main;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton5;
-    private JRadioButton radioButton7;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton4;
-    private JRadioButton radioButton6;
-    private JRadioButton radioButton8;
     private JButton usbButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox3;
+    private JComboBox comboBox5;
+    private JComboBox comboBox77;
+    private JComboBox comboBox6;
+    private JComboBox comboBox4;
+    private JComboBox comboBox2;
+    private JComboBox comboBox8;
+    private static ChronoTimer chronoTimer; //need to instantiate chronoTimer or should it be passed in from ChronoGUI?
+
+    public BackSide() {
+
+        //need to instantiate chronoTimer or should it be passed in from ChronoGUI?
+
+        chronoTimer = new ChronoTimer();
+        usbButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            chronoTimer.execute("export",null,null);
+            }
+        });
+    }
 
     public static void show(){
-        JFrame frame = new JFrame("Back Side");
+        JFrame frame = new JFrame("Back Side Goo");
         frame.setContentPane(new BackSide().main);
         frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         frame.setMinimumSize(new Dimension(500, 300));
