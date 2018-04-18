d8a3fe31b176afe3111b47d6a174e4fb6b6d93f5
Frederick Barthel <fbarthel@uwm.edu>
2018-04-18 13:25:46 -0500

    updated chronogoo: backside gooey

--- src/GUI/BackSide.java
+++ src/GUI/BackSide.java
@@ -5,12 +5,21 @@ import java.awt.*;
 
 public class BackSide {
     private JPanel main;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton5;
+    private JRadioButton radioButton7;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton4;
+    private JRadioButton radioButton6;
+    private JRadioButton radioButton8;
+    private JButton usbButton;
 
     public static void show(){
         JFrame frame = new JFrame("Back Side");
         frame.setContentPane(new BackSide().main);
         frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(500, 500));
+        frame.setMinimumSize(new Dimension(500, 300));
 //        frame.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
         frame.setLocationRelativeTo(null);
         frame.pack();
